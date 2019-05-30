660e0118f17f46ded831548bb9fd2cabdbb9ba94
nori <contact@norilog.net>
2019-05-30 04:44:18 +0000

    post

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', 'MicropostsController@index');
+
 
 // ユーザ登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
@@ -24,7 +27,9 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
+
 // ユーザ機能
-Route::group(['middleware' => ['auth']], function (){
-    Route::resource('useres', 'UsersController', ['only' => ['index', 'show']]);
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+    Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
